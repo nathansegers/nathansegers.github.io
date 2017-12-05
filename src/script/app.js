bc65671a14dd5610a0081f59dc92ec033c0fc34a
nathansegers <nathan.segers@student.howest.be>
2017-12-05 18:11:26 +0100

    Built the rough Lay-out: * Header is made * Footer is made * Section is roughly made
    SVG:
    * The SVG Map of "The Known World" of Game of Thrones is included. Built in Adobe Illustrator CC
    SVG Map Functionalities:
    * Hover over a piece of land to show the name of it.
    * Click on a piece of land to highlight it.
    
    + Added "Smooth Papyrus" font for the title. This font is a Smoother version of the one used for the SVG titles.

--- src/script/app.js
+++ src/script/app.js
@@ -2,7 +2,7 @@
 console.info("Hello there!")
 
 var all_characters = Array();
-var all_unique_cultures = ['Braavosi', 'Westeros', 'Valyrian', 'Stormlands', 'Ironborn', 'Andal', 'Northmen', 'Valemen', 'Reach', 'Dornish', 'Northern mountain clans', 'Rivermen', 'First Men', 'Westerman', 'Sistermen', 'Ghiscari', 'Crannogmen', 'Westerlands', 'Asshai', 'Rhoynar', 'Tyroshi', 'Myrish', 'Thenn', 'Free Folk', 'Dothraki', 'Norvoshi', 'Ibbenese', 'Summer Isles', 'Meereenese', 'Mountain clans', 'Astapori', 'Lysene', 'Qartheen', 'Lhazareen', 'Free folk', 'Pentoshi', 'Naathi', 'Qohor'];
+var all_unique_cultures = ['Braavosi', 'Westeros', 'Valyrian', 'Stormlands', 'Ironborn', 'Andal', 'Northmen', 'Valemen', 'Reach', 'Dornish', 'Northern mountain clans', 'Rivermen', 'First Men', 'Westerman', 'Sistermen', 'Ghiscari', 'Crannogmen', 'Westerlands', 'Asshai', 'Rhoynar', 'Tyroshi', 'Myrish', 'Thenn', 'Free Folk', 'Dothraki', 'Norvoshi', 'Ibbenese', 'Summer Isles', 'Meereenese', 'Mountain clans', 'Astapori', 'Lysene', 'Qartheen', 'Lhazareen', 'Pentoshi', 'Naathi', 'Qohor'];
 
 function APICall(type, page, page_size) {
     var xhttp = new XMLHttpRequest(),
@@ -23,13 +23,83 @@ function APICall(type, page, page_size) {
     xhttp.send();
 }
 
-function AddCulturesToList() {
-    var picker = document.querySelector('#picker');
-    for (var i = 0; i < all_unique_cultures.length; i++) {
-        var element = all_unique_cultures[i];
-        var option = document.createElement("option");
-        option.text = element;
-        picker.add(option)
+// function AddCulturesToList() {
+//     var picker = document.querySelector('#picker');
+//     for (var i = 0; i < all_unique_cultures.length; i++) {
+//         var element = all_unique_cultures[i];
+//         var option = document.createElement("option");
+//         option.text = element;
+//         picker.add(option)
+//     }
+// }
+// AddCulturesToList();
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var all_lands = document.querySelectorAll("[class*=land_]");
+    var all_pins = document.querySelectorAll("[class*=pin_]");
+    var all_titles = document.querySelectorAll("[class*=text_]");
+
+
+    HideAllPinsAndTitles();
+
+    // Function to set all items to hidden;
+    function HideAllPinsAndTitles() {
+        all_titles.forEach(function (elem) {
+            elem.classList.remove("visible");
+            elem.classList.add("invisible");
+        });
+        all_pins.forEach(function (elem) {
+            elem.classList.remove("visible");
+            elem.classList.add("invisible");            
+        });
+        all_lands.forEach(function (elem) {
+            elem.classList.remove("clicked");
+        })
     }
+
+
+    // For each land-object
+    all_lands.forEach(function (elem) {
+        // Do a function on mouse over
+        elem.addEventListener("mouseover", function () {
+            // Show the title of this element
+            ShowLandTitle(elem.id);
+        });
+        elem.addEventListener("mouseout", function() {
+            // Show the title of this element
+            HideLandTitle(elem.id);
+        })
+        elem.addEventListener("click", function() {
+            // We only want our clicked element to be visible, so we hide all the rest
+            HideAllPinsAndTitles();
+            ObjectClicked(elem);
+        });
+
+    });
+});
+
+function ShowLandTitle(land_id) {
+    var land_title = document.querySelector(".text_" + land_id);
+    land_title.classList.remove("invisible");
+    land_title.classList.add("visible");
 }
-AddCulturesToList();
+function HideLandTitle(land_id) {
+    var land_title = document.querySelector(".text_" + land_id);
+    land_title.classList.remove("visible");
+    land_title.classList.add("invisible");
+}
+function ObjectClicked(object) {
+    var object_title = document.querySelector(".text_" + object.id);
+    // Show land title
+    object.classList.add("clicked");
+    console.log(object_title.classList);
+    object_title.classList.add("visible");
+    console.log(object_title.classList);
+    
+    // Set the fill color of this object to RED
+    console.log(object);
+}
+
+
+
